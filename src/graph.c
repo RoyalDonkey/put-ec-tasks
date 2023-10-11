9579538e939383be6d4a8989e91aea2f8b25561c
Randoragon <randoragongamedev@gmail.com>
2023-10-12 00:09:11 +0200

    fix(graph): tsp_graph_read() mismanaging memory
    I was very tired, evidently, and made a whole bunch of node functions
    which are not necessary.

--- src/graph.c
+++ src/graph.c
@@ -7,18 +7,6 @@
 int _print_node(const void *ptr);
 
 
-struct tsp_node *tsp_node_create(int x, int y, int cost)
-{
-	struct tsp_node *node;
-
-	node = malloc_or_die(sizeof(struct tsp_node));
-	node->x = x;
-	node->y = y;
-	node->cost = cost;
-
-	return node;
-}
-
 struct sp_stack *tsp_graph_read(const char *fpath)
 {
 	FILE *f;
@@ -31,11 +19,12 @@ struct sp_stack *tsp_graph_read(const char *fpath)
 		error(("file not found: %s", fpath));
 	}
 	for (size_t lineno = 1;; ++lineno) {
-		int x, y, cost, n;
+		struct tsp_node node;
+		int n;
 
 		/* Parse line */
-		n = fscanf(f, "%d;%d;%d\n", &x, &y, &cost);
-		if (n == 0) {
+		n = fscanf(f, "%d;%d;%d\n", &node.x, &node.y, &node.cost);
+		if (n == EOF) {
 			info(("successfully parsed %zu lines from %s", lineno, fpath));
 			break;
 		} else if (n != 3) {
@@ -43,27 +32,15 @@ struct sp_stack *tsp_graph_read(const char *fpath)
 		}
 
 		/* Append new node to stack */
-		sp_stack_push(graph, tsp_node_create(x, y, cost));
+		sp_stack_push(graph, &node);
 	}
 	fclose(f);
 	return graph;
 }
 
-void tsp_node_destroy(struct tsp_node *node)
-{
-	free(node);
-}
-
 void tsp_graph_destroy(struct sp_stack *graph)
 {
-	sp_stack_destroy(graph, tsp_node_dtor);
-}
-
-int tsp_node_dtor(void *ptr)
-{
-	struct tsp_node *const node = ptr;
-	tsp_node_destroy(node);
-	return 0;
+	sp_stack_destroy(graph, NULL);
 }
 
 void tsp_node_print(const struct tsp_node *node)
@@ -79,6 +56,6 @@ void tsp_graph_print(const struct sp_stack *graph)
 int _print_node(const void *ptr)
 {
 	const struct tsp_node *const node = ptr;
-	printf("Node(x=%d, y=%d, cost=%d)\n", node->x, node->y, node->cost);
+	printf("Node(x=%4d, y=%4d, cost=%4d)\n", node->x, node->y, node->cost);
 	return 0;
 }
--- src/graph.h
+++ src/graph.h
@@ -14,11 +14,8 @@ struct tsp_node {
 
 
 /* Functions */
-struct tsp_node *tsp_node_create(int x, int y, int cost);
 struct sp_stack *tsp_graph_read(const char *fpath);
-void tsp_node_destroy(struct tsp_node *node);
 void tsp_graph_destroy(struct sp_stack *graph);
-int tsp_node_dtor(void *ptr);
 void tsp_node_print(const struct tsp_node *node);
 void tsp_graph_print(const struct sp_stack *graph);
 
