7b77303b67f7ac8be0a36b1f91ee03e86d295880
Randoragon <randoragongamedev@gmail.com>
2023-10-12 10:04:50 +0200

    feat(task1): Begin implementing task1

--- task1/src/main.c
+++ task1/src/main.c
@@ -1,17 +1,48 @@
 #include "../../src/tsp.h"
 #include <staple.h>
 
-int main()
+/* Constants */
+#define SEED 0
+
+
+/* Typedefs */
+typedef void (*solution_func_t)(struct tsp_graph *graph);
+
+
+/* Global variables */
+static const char *files[] = {
+	"data/TSPA.csv",
+	"data/TSPB.csv",
+	"data/TSPC.csv",
+};
+
+
+void greedy_random(struct tsp_graph *graph)
 {
-	struct tsp_graph *graph;
+}
 
-	random_seed(0);
+void run_greedy_solution(solution_func_t func)
+{
+	random_seed(SEED);
+	for (int i = 0; i < ARRLEN(files); i++) {
+		struct sp_stack *const nodes = tsp_nodes_read(files[i]);
+		struct tsp_graph *const graph = tsp_graph_create(nodes);
 
-	graph = tsp_graph_read("data/TSPA.csv");
-	tsp_graph_activate_random(graph, 100);
+		/* 200 solutions starting from each node */
+		for (int j = 0; j < 200; j++) {
+			tsp_graph_deactivate_all(graph);
+			tsp_graph_activate_node(graph, j);
+			tsp_graph_activate_random(graph, 99);
+		}
 
-	tsp_graph_print(graph);
-	tsp_graph_destroy(graph);
+		tsp_graph_destroy(graph);
+		sp_stack_destroy(nodes, NULL);
+	}
+}
 
+int main(void)
+{
+	assert(sp_is_abort());
+	run_greedy_solution(greedy_random);
 	return 0;
 }
