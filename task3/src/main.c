4c3463b5c51d5925bc9794d1bd14f43dfe958b94
Randoragon <randoragongamedev@gmail.com>
2023-11-05 12:58:21 +0100

    feat(task3): Adjust boilerplate for local search
    Now things are ready for actual implementation.

--- task3/src/main.c
+++ task3/src/main.c
@@ -2,52 +2,41 @@
 #include <limits.h>
 #include <libgen.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* Typedefs */
-typedef void (*activate_func_t)(struct tsp_graph *graph, size_t n_nodes);
+typedef void (*lsearch_func_t)(struct tsp_graph *graph);
 
 
 /* Global variables */
-static const char *files[] = {
+static const char *nodes_files[] = {
 	"data/TSPA.csv",
 	"data/TSPB.csv",
 	"data/TSPC.csv",
 	"data/TSPD.csv",
 };
-static struct sp_stack *nodes[ARRLEN(files)];
+static const char *graph_files[] = {
+	"data/best_greedy_TSPA.graph",
+	"data/best_greedy_TSPB.graph",
+	"data/best_greedy_TSPC.graph",
+	"data/best_greedy_TSPD.graph",
+};
+static struct sp_stack *nodes[ARRLEN(nodes_files)];
+static struct tsp_graph *starting_graphs[ARRLEN(graph_files)];
 
-
-void greedy_cycle_wsc(struct tsp_graph *graph, size_t target_size)
+void nothing(struct tsp_graph *graph)
 {
-	struct sp_stack *vacant = graph->nodes_vacant;
-	struct sp_stack *active = graph->nodes_active;
-
-	if (active->size == 0 && target_size != 0)
-		tsp_graph_activate_random(graph, 1);
-	if (active->size == 1 && target_size != 1) {
-		const struct tsp_node node = *(struct tsp_node*)sp_stack_peek(active);
-		const size_t idx = tsp_nodes_find_nn(vacant, &graph->dist_matrix, node);
-		tsp_graph_activate_node(graph, idx);
-	}
-	while (active->size < target_size) {
-		struct tsp_node node;
-		struct sp_stack *const rcl = tsp_graph_find_rcl(graph, 10, 0.04);
-		const struct tsp_move move = tsp_graph_find_wsc(graph, rcl, 0.5);
-		sp_stack_destroy(rcl, NULL);
-		node = *(struct tsp_node*)sp_stack_get(vacant, move.src);
-		sp_stack_remove(vacant, move.src, NULL);
-		sp_stack_insert(active, move.dest, &node);
-	}
+	return;
 }
 
-void run_greedy_algorithm(const char *algo_name, activate_func_t greedy_algo)
+void run_lsearch_algorithm(const char *label, lsearch_func_t lsearch_algo, bool random_start)
 {
-	unsigned long score_min[ARRLEN(files)];
-	unsigned long score_max[ARRLEN(files)];
-	double        score_sum[ARRLEN(files)];
-	struct tsp_graph *best_solution[ARRLEN(files)];
+	unsigned long score_min[ARRLEN(nodes_files)];
+	unsigned long score_max[ARRLEN(nodes_files)];
+	double        score_sum[ARRLEN(nodes_files)];
+	struct tsp_graph *best_solution[ARRLEN(nodes_files)];
 
-	for (size_t i = 0; i < ARRLEN(files); i++) {
+	for (size_t i = 0; i < ARRLEN(nodes_files); i++) {
 		score_min[i] = ULONG_MAX;
 		score_max[i] = 0;
 		score_sum[i] = 0.0;
@@ -55,30 +44,20 @@ void run_greedy_algorithm(const char *algo_name, activate_func_t greedy_algo)
 
 	random_seed(0);
 
-	for (size_t i = 0; i < ARRLEN(files); i++) {
+	for (size_t i = 0; i < ARRLEN(nodes_files); i++) {
 		struct tsp_graph *const graph = tsp_graph_create(nodes[i]);
 		const size_t target_size = nodes[i]->size / 2;
 		best_solution[i] = tsp_graph_create(nodes[i]);
 
-		/* 200 solutions starting from each node */
 		for (int j = 0; j < 200; j++) {
-			tsp_graph_deactivate_all(graph);
-			tsp_graph_activate_node(graph, j);
-			greedy_algo(graph, target_size);
-
-			const unsigned long score = tsp_nodes_evaluate(graph->nodes_active, &graph->dist_matrix);
-			score_min[i] = MIN(score, score_min[i]);
-			if (score > score_max[i]) {
-				score_max[i] = score;
-				tsp_graph_copy(best_solution[i], graph);
+			if (random_start) {
+				tsp_graph_deactivate_all(graph);
+				tsp_graph_activate_random(graph, target_size);
+			} else {
+				tsp_graph_copy(graph, starting_graphs[i]);
 			}
-			score_sum[i] += score;
-		}
 
-		/* 200 completely random solutions */
-		for (int j = 0; j < 200; j++) {
-			tsp_graph_deactivate_all(graph);
-			greedy_algo(graph, target_size);
+			lsearch_algo(graph);
 
 			const unsigned long score = tsp_nodes_evaluate(graph->nodes_active, &graph->dist_matrix);
 			score_min[i] = MIN(score, score_min[i]);
@@ -92,21 +71,21 @@ void run_greedy_algorithm(const char *algo_name, activate_func_t greedy_algo)
 		tsp_graph_destroy(graph);
 	}
 
-	printf("solutions found by %s:\n", algo_name);
+	printf("solutions found by %s:\n", label);
 	printf("%-20s\t%8s\t%8s\t%8s\n", "file", "min", "avg", "max");
 	for (size_t i = 0; i < ARRLEN(best_solution); i++) {
 		char instance_fpath[128], out_fname[128];
 		printf("%-20s\t%8lu\t%8lu\t%8lu\n",
-			files[i],
+			nodes_files[i],
 			score_min[i],
 			ROUND(score_sum[i] / 400),
 			score_max[i]
 		);
-		strncpy(instance_fpath, files[i], ARRLEN(instance_fpath) - 1);
+		strncpy(instance_fpath, nodes_files[i], ARRLEN(instance_fpath) - 1);
 		*strrchr(instance_fpath, '.') = '\0';  /* Trim file extension */
-		sprintf(out_fname, "results/best_%s_%s.graph", algo_name, basename(instance_fpath));
+		sprintf(out_fname, "results/best_%s_%s.graph", label, basename(instance_fpath));
 		tsp_graph_export(best_solution[i], out_fname);
-		sprintf(out_fname, "results/best_%s_%s.pdf", algo_name, basename(instance_fpath));
+		sprintf(out_fname, "results/best_%s_%s.pdf", label, basename(instance_fpath));
 		tsp_graph_to_pdf(best_solution[i], out_fname);
 		tsp_graph_destroy(best_solution[i]);
 	}
@@ -115,15 +94,16 @@ void run_greedy_algorithm(const char *algo_name, activate_func_t greedy_algo)
 int main(void)
 {
 	assert(sp_is_abort());
-	for (size_t i = 0; i < ARRLEN(files); i++) {
-		nodes[i] = tsp_nodes_read(files[i]);
+	for (size_t i = 0; i < ARRLEN(nodes_files); i++) {
+		nodes[i] = tsp_nodes_read(nodes_files[i]);
+		starting_graphs[i] = tsp_graph_import(graph_files[i]);
 	}
 
-	/* run_greedy_algorithm("2-regret", greedy_cycle_2regret); */
-	/* run_greedy_algorithm("weighted-sum-criterion", greedy_cycle_wsc); */
+	run_lsearch_algorithm("big-fat-nothing", nothing, false);
 
-	for (size_t i = 0; i < ARRLEN(files); i++) {
+	for (size_t i = 0; i < ARRLEN(nodes_files); i++) {
 		sp_stack_destroy(nodes[i], NULL);
+		tsp_graph_destroy(starting_graphs[i]);
 	}
 	return 0;
 }
